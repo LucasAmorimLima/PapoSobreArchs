1ee04567431930e73b278ec1209316b41022db7a
Lucas Amorim Lima <lucasasmorimlima17@gmail.com>
2022-03-16 09:41:16 -0300

    segundo modelo

--- src/segundoModelo/controller/userController.ts
+++ src/segundoModelo/controller/userController.ts
@@ -1,9 +1,26 @@
 import { UserService } from "../service/userService";
+import { HttpRequest, HttpResponse, HttpStatusCodes } from "./types";
 
 export class UserController {
     constructor(
         private userService: UserService
-    ) {
-        
+    ) { }
+
+    async create(httpRequest: HttpRequest, httpResponse: HttpResponse) {
+        try {
+            const { body } = httpRequest
+
+            const response = await this.userService.create(body)
+
+            return {
+                httpStatusCode: HttpStatusCodes.created.code,
+                body: response
+            }
+        } catch (error: any) {
+            return {
+                httpStatusCode: HttpStatusCodes.serverError.code,
+                body: error.message
+            }
+        }
     }
 }
